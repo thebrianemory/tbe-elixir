b116fa68a8a39dac536a15fc3ac61e12e64daf7e
thebrianemory <the@brianemory.com>
2017-08-11 22:24:51 -0700

    Create app_nav and add to app.html.eex

--- test/tbe_elixir_web/views/page_view_test.exs
+++ test/tbe_elixir_web/views/page_view_test.exs
@@ -1,3 +1,10 @@
 defmodule TbeElixirWeb.PageViewTest do
   use TbeElixirWeb.ConnCase, async: true
+
+  test "shows blog, projects, and FAQ on navbar", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Blog"
+    assert html_response(conn, 200) =~ "Projects"
+    assert html_response(conn, 200) =~ "FAQ"
+  end
 end
