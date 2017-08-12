5c1c89552c5b9d71f69923f9f547a26069911cfe
thebrianemory <the@brianemory.com>
2017-08-12 07:14:50 -0700

    Add more assertions for the other navbar pieces

--- test/tbe_elixir_web/partials/navigation_test.exs
+++ test/tbe_elixir_web/partials/navigation_test.exs
@@ -3,8 +3,12 @@ defmodule TbeElixirWeb.NavigationPartialTest do
 
   test "shows blog, projects, and FAQ on navbar", %{conn: conn} do
     conn = get conn, "/"
+    assert html_response(conn, 200) =~ "<a class=\"navbar-brand page-scroll nav-text blue-text\" href=\"page-top\">"
     assert html_response(conn, 200) =~ "<a class=\"page-scroll blue-text\" href=\"#blog\">"
     assert html_response(conn, 200) =~ "<a class=\"page-scroll blue-text\" href=\"#projects\">"
     assert html_response(conn, 200) =~ "<a class=\"page-scroll blue-text\" href=\"#faq\">"
+    assert html_response(conn, 200) =~ "<a href=\"https://twitter.com/thebrianemory\" target=\"_blank\">"
+    assert html_response(conn, 200) =~ "<a href=\"https://www.linkedin.com/in/thebrianemory\" target=\"_blank\">"
+    assert html_response(conn, 200) =~ "<a href=\"https://github.com/thebrianemory\" target=\"_blank\">"
   end
 end
