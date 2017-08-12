77c7505b0e32e004596418a1529b25d9a70c995d
thebrianemory <the@brianemory.com>
2017-08-12 06:55:10 -0700

    Extract nav test, rename nav partial
    I moved the test for the navigation partial to its own test within a new
    directory called partials. I like this better for keeping the tests of
    the different partials I will be using separate. Otherwise, all my tests
    for the different partials that show on the homepage would be stuffed
    into the page_controller_test.
    
    I also renamed the navigation partial from app_nav to _navigation. I
    prefer this as it makes it a bit more obvious that it's a partial and
    not its own page.

---
+++ test/tbe_elixir_web/partials/navigation_test.exs
@@ -0,0 +1,10 @@
+defmodule TbeElixirWeb.NavigationPartialTest do
+  use TbeElixirWeb.ConnCase
+
+  test "shows blog, projects, and FAQ on navbar", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Blog"
+    assert html_response(conn, 200) =~ "Projects"
+    assert html_response(conn, 200) =~ "FAQ"
+  end
+end
