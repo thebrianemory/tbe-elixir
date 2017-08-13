dc54ae4c18433dfbcb5ba1492fb4dd1e45de032f
thebrianemory <the@brianemory.com>
2017-08-13 12:26:31 -0700

    Begin creating functions for getting posts
    Started building the code needed to get my blog posts from my my Medium
    RSS feed. I am able to use my Medium JSON URL, and using RSS2JSON get
    back the data in JSON format. I then use HTTPoison and Poison to break
    that down into a usable format.
    
    Currently have the get_post_date/1 function built and I am working on
    getting the functions called within it to format the data. Just finished
    the formatting of the date.

--- lib/tbe_elixir_web/controllers/page_controller.ex
+++ lib/tbe_elixir_web/controllers/page_controller.ex
@@ -1,7 +1,58 @@
 defmodule TbeElixirWeb.PageController do
   use TbeElixirWeb, :controller
+  use Timex
 
   def index(conn, _params) do
-    render conn, "index.html"
+    posts = get_blog_posts
+    render conn, "index.html", posts: posts
+  end
+
+  defp get_blog_posts do
+    blog_url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.brianemory.com%2Ffeed&api_key=#{System.get_env("RSS2JSON")}&count=3"
+json_data = HTTPoison.get!(blog_url)
+    %{feed: _, items: items, status: _} = Poison.Parser.parse!(json_data.body, keys: :atoms)
+    posts = Enum.map(items, fn(item) ->
+      get_post_data(item)
+    end)
+    require IEx; IEx.pry
+  end
+
+  defp get_post_data(item) do
+    post = %{
+      title:   get_title(item.title),
+      date:    get_date(item.pubDate),
+      link:    get_link(item.link),
+      heading: get_heading(item.content),
+      content: get_content(item.content)
+    }
+  end
+
+  defp get_title(title) do
+    title
+  end
+
+
+  defp get_date(date) do
+    parsed_date = Timex.parse!(date, "%Y-%m-%d %H:%M:%S", :strftime)
+    today = Timex.shift(Timex.to_naive_datetime(Timex.now), days: -1)
+
+    case parsed_date > today do
+      true ->
+        Timex.from_now(parsed_date)
+      false ->
+        Timex.format!(new_date, "%b %d, %Y", :strftime)
+    end
+  end
+
+  defp get_link(link) do
+    link
+  end
+
+  defp get_heading(content) do
+    content
+  end
+
+  defp get_content(content) do
+    content
   end
 end
--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule TbeElixir.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 0.12"}
+      {:httpoison, "~> 0.12"},
+      {:poison, "~> 3.1"},
+      {:timex, "~> 3.1"}
     ]
   end
 
