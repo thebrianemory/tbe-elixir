32aacf17ec48d929224e6d32488fa3f5983aee0f
thebrianemory <the@brianemory.com>
2017-08-13 20:13:30 -0700

    Add faq test and partial

---
+++ test/tbe_elixir_web/partials/faq_test.exs
@@ -0,0 +1,13 @@
+defmodule TbeElixirWeb.FAQPartialTest do
+  use TbeElixirWeb.ConnCase
+
+  test "shows heading for FAQ", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "<section class=\"faq-section\" id=\"faq\">"
+  end
+
+  test "shows GitHub button", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "<h4>How tall are you?</h4>"
+  end
+end
