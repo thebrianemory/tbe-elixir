284d42bdbd13c66661a0bd1c9a91fd58b02d2ce4
thebrianemory <the@brianemory.com>
2017-08-13 21:25:37 -0700

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/tbe_elixir_web/channels/user_socket.ex
+++ lib/tbe_elixir_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule TbeElixirWeb.UserSocket do
   # channel "room:*", TbeElixirWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
