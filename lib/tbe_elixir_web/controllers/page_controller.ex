fb94a1c0be636b8faeeb040dcd28f59b6b4bfb30
thebrianemory <the@brianemory.com>
2017-08-15 11:17:13 -0700

    Update app to use GetMedium package
    Updated to use my GetMedium package for grabbing blog posts from Medium.

--- lib/tbe_elixir_web/controllers/page_controller.ex
+++ lib/tbe_elixir_web/controllers/page_controller.ex
@@ -1,11 +1,9 @@
 defmodule TbeElixirWeb.PageController do
   use TbeElixirWeb, :controller
 
-  alias TbeElixir.Blog
-
   def index(conn, _params) do
     blog_url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.brianemory.com%2Ffeed&api_key=#{System.get_env("RSS2JSON")}&count=3"
-    posts = Blog.blog_posts_truncated(blog_url)
+    posts = GetMedium.Truncated.blog_posts(blog_url)
     render conn, "index.html", posts: posts
   end
 end
--- mix.exs
+++ mix.exs
@@ -41,10 +41,7 @@ defmodule TbeElixir.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 0.12"},
-      {:poison, "~> 3.1"},
-      {:timex, "~> 3.1"},
-      {:html_sanitize_ex, "~> 1.3.0-rc3"}
+      {:get_medium, "~> 0.1.0"}
     ]
   end
 
