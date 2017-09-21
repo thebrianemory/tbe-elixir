e1492511e04b8d0d33bea4d04719aa4baf94cdfa
thebrianemory <the@brianemory.com>
2017-09-20 22:54:26 -0700

    Set up ability to have a subdomain

---
+++ lib/tbe_elixir_web/subdomain_router.ex
@@ -0,0 +1,17 @@
+defmodule TbeElixirWeb.SubdomainRouter do
+  use TbeElixirWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", TbeElixirWeb.Subdomain do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+end
