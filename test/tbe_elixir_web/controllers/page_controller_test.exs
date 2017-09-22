c192329d142df73e119627ffff428649e82a73fc
thebrianemory <the@brianemory.com>
2017-09-21 22:59:54 -0700

    Fix test by passing full URL (not a good solution)
    After adding the subdomain router yesterday, realized today that all of
    my tests are broken. After looking into it a bit more, the tests are
    being sent to www.example.com. Since www is a subdomain, this is sending
    all of my test requests to the wrong router causing them to fail. I can
    get the test to pass if I manually pass in the full address of
    http://localhost:4000 and that is what I did for this test. However,
    this is not a good solution as it does not fix the issue. Going to have
    to look into this more, namely, catching the subdomains of www and
    sending them through the regular router.

--- test/tbe_elixir_web/controllers/page_controller_test.exs
+++ test/tbe_elixir_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule TbeElixirWeb.PageControllerTest do
   use TbeElixirWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
+    conn = get conn, "http://localhost:4000/"
     assert html_response(conn, 200) =~ "Brian Emory"
   end
 end
