4d71f0a10a6c613fa0aac3df3e0adb854b5848d7
thebrianemory <the@brianemory.com>
2017-10-21 14:46:01 -0700

    Update get_subdomain to properly get subdomain

--- lib/tbe_elixir_web/plugs/subdomain.ex
+++ lib/tbe_elixir_web/plugs/subdomain.ex
@@ -1,30 +1,30 @@
-defmodule TbeElixirWeb.Plug.Subdomain do
-  import Plug.Conn
+  defmodule TbeElixirWeb.Plug.Subdomain do
+    import Plug.Conn
 
-  @doc false
-  def init(default), do: default
+    @doc false
+    def init(default), do: default
 
-  @doc """
-  This plug ensures that the subdomain of "www" gets routed to the standard page_controller, subdomains of "til" get routed to the subdomain page_controller, no subdomain also goes to the standard page_controller, and everything else goes to a 404.
-  """
-  def call(conn, router) do
-    {root_host, subdomain, host} = get_subdomain(conn.host)
+    @doc """
+    This plug ensures that the subdomain of "www" gets routed to the standard page_controller, subdomains of "til" get routed to the subdomain page_controller, no subdomain also goes to the standard page_controller, and everything else goes to a 404.
+    """
+    def call(conn, router) do
+      subdomain = get_subdomain(conn.host)
 
-    case subdomain do
-      subdomain when byte_size(subdomain) == 0 -> conn
-      subdomain when subdomain == "www" -> conn |> put_private(:subdomain, subdomain)
-      subdomain when subdomain == "til" ->
-        conn
-        |> put_private(:subdomain, subdomain)
-        |> router.call(router.init({}))
-        |> halt
-      subdomain -> conn |> put_private(:subdomain, "host: " <> host <> " - subdomain: " <> subdomain <> " - root_host:  " <> root_host <> " --no luck")
+      case subdomain do
+        subdomain when byte_size(subdomain) == 0 -> conn
+        subdomain when subdomain == "www" -> conn |> put_private(:subdomain, subdomain)
+        subdomain when subdomain == "til" ->
+          conn
+          |> put_private(:subdomain, subdomain)
+          |> router.call(router.init({}))
+          |> halt
+        subdomain -> conn |> put_private(:subdomain, subdomain <> " --no luck")
+      end
     end
-  end
 
   defp get_subdomain(host) do
-    root_host = TbeElixirWeb.Endpoint.config(:url)[:host]
-    subdomain = String.replace(host, ~r/.?#{root_host}/, "")
-    {root_host, subdomain, host}
+    host
+    |> String.split(".")
+    |> hd
   end
 end
