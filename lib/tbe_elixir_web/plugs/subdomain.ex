e7711e269c4a078a538ff99e629a714ac3bdfc8b
thebrianemory <the@brianemory.com>
2017-10-21 14:14:27 -0700

    Update debug text being shown

--- lib/tbe_elixir_web/plugs/subdomain.ex
+++ lib/tbe_elixir_web/plugs/subdomain.ex
@@ -8,7 +8,7 @@ defmodule TbeElixirWeb.Plug.Subdomain do
   This plug ensures that the subdomain of "www" gets routed to the standard page_controller, subdomains of "til" get routed to the subdomain page_controller, no subdomain also goes to the standard page_controller, and everything else goes to a 404.
   """
   def call(conn, router) do
-    {root_host, subdomain} = get_subdomain(conn.host)
+    {root_host, subdomain, host} = get_subdomain(conn.host)
 
     case subdomain do
       subdomain when byte_size(subdomain) == 0 -> conn
@@ -18,13 +18,13 @@ defmodule TbeElixirWeb.Plug.Subdomain do
         |> put_private(:subdomain, subdomain)
         |> router.call(router.init({}))
         |> halt
-      subdomain -> conn |> put_private(:subdomain, subdomain <> "/" <> root_host <> " --no luck")
+      subdomain -> conn |> put_private(:subdomain, "host: " <> host <> " - subdomain: " <> subdomain <> " - root_host:  " <> root_host <> " --no luck")
     end
   end
 
   defp get_subdomain(host) do
     root_host = TbeElixirWeb.Endpoint.config(:url)[:host]
     subdomain = String.replace(host, ~r/.?#{root_host}/, "")
-    {root_host, subdomain}
+    {root_host, subdomain, host}
   end
 end
