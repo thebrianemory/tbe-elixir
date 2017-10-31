0a630f9dc4bbe9f2c7c82ceda3f14ba84dbcbc58
thebrianemory <the@brianemory.com>
2017-10-31 12:08:54 -0700

    Revert previous commit

--- apps/tbe_elixir/config/prod.exs
+++ apps/tbe_elixir/config/prod.exs
@@ -18,8 +18,7 @@ config :tbe_elixir, TbeElixirWeb.Endpoint,
   url: [scheme: "https", host: "thebrianemory.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  check_origin: ["https://www.brianemory.com", "https://til.brianemory.com"]
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
   config :tbe_elixir, TbeElixir.Repo,
