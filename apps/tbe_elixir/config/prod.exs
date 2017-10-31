5da9cc92ee63f1579d44666266f2d0307b1126cf
thebrianemory <the@brianemory.com>
2017-10-31 12:06:21 -0700

    Add check_origin endpoint

--- apps/tbe_elixir/config/prod.exs
+++ apps/tbe_elixir/config/prod.exs
@@ -18,7 +18,8 @@ config :tbe_elixir, TbeElixirWeb.Endpoint,
   url: [scheme: "https", host: "thebrianemory.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  check_origin: ["https://www.brianemory.com", "https://til.brianemory.com"]
 
 # Configure your database
   config :tbe_elixir, TbeElixir.Repo,
