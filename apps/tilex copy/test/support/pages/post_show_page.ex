d344f25c128cd7d646d8ce2c52aba66613cdebf7
thebrianemory <the@brianemory.com>
2017-10-31 09:44:51 -0700

    Remove tilex as a submodule
    This is a multi-commit process to remove tilex as a submodule. Since it
    wasn't be

---
+++ apps/tilex copy/test/support/pages/post_show_page.ex
@@ -0,0 +1,49 @@
+defmodule Tilex.Integration.Pages.PostShowPage do
+  use Wallaby.DSL
+
+  def navigate(session, post) do
+    visit(session, "/posts/#{post.slug}")
+  end
+
+  def ensure_page_loaded(session, title) do
+    session
+    |> Browser.find(Query.css("article.post"))
+
+    session
+    |> Browser.find(Query.css("h1", text: title))
+
+    session
+  end
+
+  def ensure_info_flash(session, message) do
+    session
+    |> Browser.find(Query.css(".alert-info", text: message))
+
+    session
+  end
+
+  def expect_post_attributes(session, attrs \\ %{}) do
+    expected_title = Map.fetch!(attrs, :title)
+    expected_body = Map.fetch!(attrs, :body)
+    expected_channel = Map.fetch!(attrs, :channel)
+    expected_likes_count = Map.fetch!(attrs, :likes_count) |> to_string()
+
+    session
+    |> Browser.find(Query.css(".post h1", text: expected_title))
+
+    session
+    |> Browser.find(Query.css(".post .copy", text: expected_body))
+
+    session
+    |> Browser.find(Query.css(".post aside .post__tag-link", text: String.upcase(expected_channel)))
+
+    session
+    |> Browser.find(Query.css(".js-like-action", text: expected_likes_count))
+
+    session
+  end
+
+  def click_edit(session) do
+    click(session, Query.link("edit"))
+  end
+end
