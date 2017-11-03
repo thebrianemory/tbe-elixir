523fb35006ee650f3487d8f7717ef57a71a4f349
thebrianemory <the@brianemory.com>
2017-11-03 13:20:39 -0700

    Remove code
    Not sure if any of this is going to accomplish anything. Trying to take
    things piece by piece until I can again push the code to heroku.

--- apps/tbe_elixir/lib/tbe_elixir/repo.ex
+++ apps/tbe_elixir/lib/tbe_elixir/repo.ex
@@ -5,7 +5,4 @@ defmodule TbeElixir.Repo do
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
   """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
-  end
 end
